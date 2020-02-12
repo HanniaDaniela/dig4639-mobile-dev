a38816204613292cac701cb1d2ee89455edb3618
HanniaDaniela <hanniamorales.hm@gmail.com>
2020-02-12 12:35:58 -0500

    closes #95

--- ice/w6a/src/index.js
+++ ice/w6a/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  render() {
+    return (
+      <button
+      className={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+
     constructor(props) {
         super(props);
         this.state = {
@@ -23,8 +33,19 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+      <Square 
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />
+      );
     }
   
     render() {
