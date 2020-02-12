0f355a94a0e9fea2edf5d2d762531d469b4478ed
HanniaDaniela <hanniamorales.hm@gmail.com>
2020-02-12 12:11:57 -0500

    Updates

---
+++ precache-manifest.8085cb979e69375e0cdd33d90b592d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec43afe800f84b147c5b97d687b1862",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "7305be75d6b2509f03db",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7305be75d6b2509f03db",
+    "url": "/dig4639-mobile-dev/static/js/main.530ebd2a.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
