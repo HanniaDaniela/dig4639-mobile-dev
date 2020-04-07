73b43da234dc9bff7a9742c0ab206fe24148b007
HanniaDaniela <hanniamorales.hm@gmail.com>
2020-04-07 19:41:59 -0400

    Lab 6 Done

--- labs/6/App.js
+++ labs/6/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { StyleSheet, Text, View, } from 'react-native'
 import { Card, Button } from 'react-native-elements'
 import questions from './questions.json'
